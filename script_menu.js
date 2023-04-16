04d78c66909122aaa08ed2e962d3e5d82ceb2148
CCW <40672745+wangching@users.noreply.github.com>
2023-04-16 16:41:21 -0400

    2
    2commit

---
+++ script_menu.js
@@ -0,0 +1,31 @@
+// let list = document.querySelectorAll(".list li");
+// let box = document.querySelectorAll(".box");
+
+// list.forEach(function (el, index) {
+//   if (index === 0) {
+//     el.style.color = "palegreen";
+//     box.forEach(function (el2) {
+//       el2.style.display = "none";
+//     });
+//     document
+//       .querySelectorAll(el.dataset.color)
+//       .forEach(function (el3) {
+//         el3.style.display = "flex";
+//       });
+//   }
+
+//   el.addEventListener("click", function (btn) {
+//     list.forEach(function (el1) {
+//       el1.style.color = "#fff";
+//     });
+//     btn.target.style.color = "palegreen";
+//     box.forEach(function (el2) {
+//       el2.style.display = "none";
+//     });
+//     document
+//       .querySelectorAll(btn.target.dataset.color)
+//       .forEach(function (el3) {
+//         el3.style.display = "flex";
+//       });
+//   });
+// });
